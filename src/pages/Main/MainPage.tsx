c33a06abc1831c4f077b1b683988c839ccbca1d7
namu2267 <namu2267@gmail.com>
2024-03-15 03:38:04 +0900

    main페이지에 캐러셀 추가

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,22 +1,13 @@
 import React from "react";
+import Carousel from "../../components/Carousel/Carousel";
 
 export default function MainPage() {
   return (
     <div>
-      메인 페이지 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Earum blanditiis, nihil iusto laborum corporis voluptatum repellat
-      tenetur! Quae sint delectus quisquam eligendi fugit assumenda incidunt
-      iusto, hic sapiente unde quaerat. 메인 페이지 Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Earum blanditiis, nihil iusto laborum
-      corporis voluptatum repellat tenetur! Quae sint delectus quisquam eligendi
-      fugit assumenda incidunt iusto, hic sapiente unde quaerat.메인 페이지
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
-      blanditiis, nihil iusto laborum corporis voluptatum repellat tenetur! Quae
-      sint delectus quisquam eligendi fugit assumenda incidunt iusto, hic
-      sapiente unde quaerat.메인 페이지 Lorem ipsum dolor, sit amet consectetur
+      <Carousel /> 메인 페이지 Lorem ipsum dolor, sit amet consectetur
       adipisicing elit. Earum blanditiis, nihil iusto laborum corporis
       voluptatum repellat tenetur! Quae sint delectus quisquam eligendi fugit
-      assumenda incidunt iusto, hic sapiente unde quaerat.메인 페이지 Lorem
+      assumenda incidunt iusto, hic sapiente unde quaerat. 메인 페이지 Lorem
       ipsum dolor, sit amet consectetur adipisicing elit. Earum blanditiis,
       nihil iusto laborum corporis voluptatum repellat tenetur! Quae sint
       delectus quisquam eligendi fugit assumenda incidunt iusto, hic sapiente
@@ -251,7 +242,17 @@ export default function MainPage() {
       ipsum dolor, sit amet consectetur adipisicing elit. Earum blanditiis,
       nihil iusto laborum corporis voluptatum repellat tenetur! Quae sint
       delectus quisquam eligendi fugit assumenda incidunt iusto, hic sapiente
-      unde quaerat.
+      unde quaerat.메인 페이지 Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Earum blanditiis, nihil iusto laborum corporis
+      voluptatum repellat tenetur! Quae sint delectus quisquam eligendi fugit
+      assumenda incidunt iusto, hic sapiente unde quaerat.메인 페이지 Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Earum blanditiis,
+      nihil iusto laborum corporis voluptatum repellat tenetur! Quae sint
+      delectus quisquam eligendi fugit assumenda incidunt iusto, hic sapiente
+      unde quaerat.메인 페이지 Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Earum blanditiis, nihil iusto laborum corporis
+      voluptatum repellat tenetur! Quae sint delectus quisquam eligendi fugit
+      assumenda incidunt iusto, hic sapiente unde quaerat.
     </div>
   );
 }
