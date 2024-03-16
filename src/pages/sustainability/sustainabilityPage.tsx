de629af8edb537a9a856108159fd55d1d168d2ec
namu2267 <namu2267@gmail.com>
2024-03-17 01:30:05 +0900

    chore:컴포넌트 이름수정

--- src/pages/sustainability/sustainabilityPage.tsx
+++ src/pages/sustainability/sustainabilityPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ReasonPage() {
+export default function SustainabilityPage() {
   return (
     <div>
       탄소중립을 지켜야하는 이유 페이지 / 친환경 활동 소개 및 중요성 강조
