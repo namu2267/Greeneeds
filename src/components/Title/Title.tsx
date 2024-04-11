7c2ce2d491f80651866b807c45c0da52863628ac
namu2267 <namu2267@gmail.com>
2024-04-11 20:15:25 +0900

    fix:Title 컴포넌트 div에 className추가, scss에도 추가

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -12,13 +12,13 @@ export default function Title({
     <div>
       {titleImgSrc ? (
         // titleImgSrc가 존재하는 경우
-        <div className="titleWrapper">
+        <div className="titleImgWrapper">
           <img src={titleImgSrc} alt="" />
           <h1>{title}</h1>
         </div>
       ) : (
         // titleImgSrc가 존재하지 않는 경우
-        <div>
+        <div className="titleWrapper">
           <h1>{title}</h1>
         </div>
       )}
